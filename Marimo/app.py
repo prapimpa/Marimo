04c1f575ae8efadb1afb72bd9974c7e141a7e2f6
prapimpa <prapimpagowit15@gmail.com>
2026-01-29 12:02:24 +0700

    Update app.py

--- Marimo/app.py
+++ Marimo/app.py
@@ -1027,5 +1027,6 @@ def normalize_text(text):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
